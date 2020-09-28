b113d40288b67bdd8695bd2dfefc879301b4d842
xiaoquanquanxin <xxkl123@126.com>
2020-09-28 19:58:49 +0800

    初始化

---
+++ src/store/a.ts
@@ -0,0 +1,41 @@
+export const routeMap = {
+	// 用户列表
+	"user": true,
+	//  病人信息
+	"patientInfo": true,
+	//	确认入院
+	"confirmHospital":true,
+	//  筛查
+	"screening": true,
+	//	营养评估
+	"assess": true,
+	//	营养干预
+	"intervene": true,
+	//	监测
+	"monitor": true,
+	//	病程记录
+	"treatmentCourse": true,
+	//	活动小结
+	"activitySummary": true,
+
+	// 处方审核列表
+	"prescription": true,
+	//	处方详情
+	"prescriptionDetail": true,
+	// 配置单
+	"configuration": true,
+	// 配置单详情
+	"configurationDetail": true,
+	// 收计费
+	"charge": true,
+	//	缴费
+	"pay": true,
+	//	计费
+	"billing": true,
+	// 系统设置
+	"system": true,
+	// 修改密码
+	"changePassword": true,
+	// 消息列表
+	"message": true,
+};
