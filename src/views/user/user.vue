dec1c818655aaa919aca644bb3e9fbdd1fc049db
xiaoquanquanxin <xxkl123@126.com>
2020-09-28 17:20:46 +0800

    初始化

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,10 +1,32 @@
 <template>
-    <router-view/>
+    <div>
+        <a-menu v-model="current" mode="horizontal">
+            <a-menu-item key="1">
+                <a-icon type="mail"/>
+                第一项
+            </a-menu-item>
+            <a-menu-item key="2">
+                <a-icon type="mail"/>
+                第二项
+            </a-menu-item>
+            <a-menu-item key="3">
+                <a-icon type="mail"/>
+                第三项
+            </a-menu-item>
+        </a-menu>
+        <router-view/>
+    </div>
 </template>
 <script>
     export default {
-        name: 'UserManagement',
+        name: 'user',
+        data(){
+            return {
+                current: ['1'],
+            };
+        },
     };
 </script>
+
 <style lang="stylus">
 </style>
